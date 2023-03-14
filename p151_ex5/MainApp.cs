2cc40f1ea3869811daafecf4672ffea28e244cc0
Armei7721 <hgjong23@naver.com>
2023-03-14 15:43:29 +0900

    20230314 1543 : 문제 3 4번 숙제하기로

--- p151_ex5/MainApp.cs
+++ p151_ex5/MainApp.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using static System.Math;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,8 @@ namespace p151_ex5
 {
     class MainApp
     {
-        static void Main(string[] args)
+      
+            static void Main(string[] args)
         {
             
             Console.Write("세개의 정수 중 첫번째 정수를 입력해주세요 : ");
@@ -21,14 +23,13 @@ namespace p151_ex5
             string c = Console.ReadLine();
             int nb3 = int.Parse(c);
             int[] arr= { nb1, nb2, nb3 };
-            int max = arr.Max();
-            int min = arr.Min();
+            int max = arr.Max(); //int max = Math.Max(Math.Max(nb1, nb2), nb3);
+            int min = arr.Min(); //int min = Math.Min(Math.Min(nb1, nb2), nb3);
             int sum = arr.Sum();
             double average = arr.Average();
             Console.WriteLine($"입력하신 {nb1},{nb2},{nb3} 중 최대값은 {max} 이며, 최소값은 {min} 입니다.");
             Console.WriteLine($"입력하신 {nb1},{nb2},{nb3} 중 합은 {sum} 이며, 평균은 {average} 입니다.");
-            
-            
+             
         }
     }
 }
---
+++ p161_ex2/MainApp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p161_ex2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("정수 2개와 연산기호를 입력해주세요!");
+            Console.Write("첫 번째 정수를 입력해주세요. : ");
+            string a =Console.ReadLine();
+            Console.Write("두 번째 정수를 입력해주세요. : ");
+            string b = Console.ReadLine();
+            Console.Write("연산기호를 입력해주세요. ex)+,-,*,/,% : ");
+            string c = Console.ReadLine();
+
+            int nb1 = int.Parse(a);
+            int nb2 = int.Parse(b);
+            int d = 0;
+            switch (c)
+            {
+                case "+":
+                    d = nb1 + nb2;
+                    break;
+                case "-":
+                    d = nb1 - nb2;
+                    break;
+                case "*":
+                    d = nb1 * nb2;
+                    break;
+                case "/":
+                    d = nb1 / nb2; 
+                    break;
+                case "%":
+                    d = nb1 % nb2;
+                    break;
+                default :
+                    Console.WriteLine("입력하신 연산기호는 잘못되었습니다. 확인 후 다시 실행해 주세요.");
+                    return;
+            }
+            Console.WriteLine($"입력하신 {nb1}%{nb2}의 결과값은 {d} 입니다.");
+
+
+        }
+    }
+}
