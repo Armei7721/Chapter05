6072715dd81e73e19b3e7a406b207ff13dacf0ee
Armei7721 <hgjong23@naver.com>
2023-03-15 14:07:54 +0900

    20230315 1340

--- p161_ex4/MainApp.cs
+++ p161_ex4/MainApp.cs
@@ -12,27 +12,27 @@ namespace p161_ex4
         {
             Console.Write("알고싶은 월을 입력해주세요. : ");
             string m = Console.ReadLine();
-           
+            string d = "";
             switch (m)
             {
                 case "1": case "2":
                 case "12":
-                Console.WriteLine($"입력하신 {m}월은 겨울 입니다. ");
+                    d = "겨울";
                     break;
                 case "3": 
                 case "4": 
                 case "5" :
-                        Console.WriteLine($"입력하신 {m}월은 봄 입니다. ");
+                    d = "봄"; 
                     break;
                 case "6": 
                 case "7": 
                 case "8":
-                        Console.WriteLine($"입력하신 {m}월은 여름 입니다. ");
+                    d = "여름"; 
                     break;
                 case "9":
                 case "10":
                 case "11":
-                    Console.WriteLine($"입력하신 {m}월은 가을 입니다. ");
+                    d = "가을";
                     break;
                 default:
                     Console.WriteLine("정상적인 입력값이 아닙니다. 확인 후 다시 실해해 주세요.");
@@ -41,6 +41,7 @@ namespace p161_ex4
 
 
             }
+            Console.WriteLine($"입력하신 {m}월은 {d} 입니다. ");
 
         }
 
